40750f3d0ad254d1c59884375afb8ffc4d2abca8
David Joshua <davidjosh_87@yahoo.com>
2018-04-03 19:39:04 +0700

    dictionary-sort-afif

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,25 @@
+/*jshint esversion:6*/
 const readline = require('readline');
-// your code here to initialize the program and take user input
+const arrOfWord = ['makan', 'duduk', 'tidur', 'terbang'];
+const arrOfName = ['anggi', 'angga', 'ani', 'adi'];
 
-module.exports = Dictionary
+function dictionary(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+      let first = arr[i];
+      if (arr[i] > arr[j]) {
+        arr[i] = arr[j];
+        arr[j] = first;
+      }
+    }
+  }
+  return arr;
+}
+
+
+
+
+console.log(dictionary(arrOfWord)); //duduk,makan,terbang,tidur
+console.log(dictionary(arrOfName)); //adi,angga,anggi,ani
+
+module.exports = dictionary;
