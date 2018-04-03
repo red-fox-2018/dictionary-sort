178f8e84384c23d09b45f01f7dd54ee0aa125996
fastabiqul1993 <fastabiqul1993@gmail.com>
2018-04-03 20:26:25 +0700

    AdhyFastabiqulKhoirot

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,25 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
 
-module.exports = Dictionary
+function Dictionary(arrWords) {
+
+    for (let i = arrWords.length-1; i >= 0; i--) {
+        
+        for (let j = arrWords.length-1; j >= 0; j--) {
+            
+            if(arrWords[j] < arrWords[j-1]) {
+
+                let temp = arrWords[j]
+                arrWords[j] = arrWords[j-1]
+                arrWords[j-1] = temp 
+            }
+        }
+    }
+
+    return arrWords
+}
+
+console.log(Dictionary(['makan','duduk','tidur','terbang']));
+console.log(Dictionary(['anggi','angga','ani','adi']));
+
+// module.exports = Dictionary
