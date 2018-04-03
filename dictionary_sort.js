e53f5536ee9d5e866e81458a38f2b20f945eb903
Renal Apriansyah <renal>
2018-04-03 16:09:36 +0700

    add dictionary sort task

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,21 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
+function sortWords(arr){
+    for(let i=0;i<arr.length;i++){
+        for(let j=i+1;j<arr.length;j++){
+            if(arr[i].toLowerCase()>arr[j].toLowerCase()){
+                var temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
+            }
+        }
+    }
+    return arr.join(',')
+}
 
-module.exports = Dictionary
+var arrOfWord = ['makan','duduk','tidur','terbang']
+console.log(sortWords(arrOfWord))
+var arrOfWord = ['anggi', 'angga', 'ani', 'adi']
+console.log(sortWords(arrOfWord))
+
+// module.exports = Dictionary
