e7d803abf588c04b7a244f823b44beba04944cc6
soni wijaya <soniwijaya59@yahoo.co.id>
2018-04-03 17:18:49 +0700

    dictionary fix

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,17 @@
-const readline = require('readline');
-// your code here to initialize the program and take user input
+function dictionary(array){
+    var result = ''
+    for(let i=0;i<array.length;i++){
+        for(let k=i+1;k<array.length;k++){
+            if(array[i].toLowerCase()>array[k].toLowerCase()){
+                var tmp = array[i]
+                array[i] = array[k]
+                array[k] = tmp
+            }
+        }
+        result += array[i]+','
+    }
+    return result.slice(0,result.length-1)
+}
 
-module.exports = Dictionary
+console.log(dictionary(['Makan','duduk','tidur','terBAng']))
+console.log(dictionary(['anggi','angga','ani','adi']))
